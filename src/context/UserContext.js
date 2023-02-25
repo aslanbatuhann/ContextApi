24370962c448e37752048253ea36fa57641be9f4
Batuhan Aslan <batuhanaslan9601@gmail.com>
2023-02-25 21:12:53 +0300

    React context api example

---
+++ src/context/UserContext.js
@@ -0,0 +1,16 @@
+import { createContext, useContext, useState } from "react";
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+
+    const values = {user, setUser};
+
+    return <UserContext.Provider value={values}>{children}</UserContext.Provider>
+};
+
+const useUser = () => useContext(UserContext);
+
+export  { useUser, UserProvider };
